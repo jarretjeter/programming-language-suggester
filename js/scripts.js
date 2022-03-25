3efc485417c3e47dd353f2d2d4ad0c30de32a783
Jarret Jeter <jarretjeter@yahoo.com>
2022-03-25 13:58:30 -0700

    define initial variables in 'scripts.js' file

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,13 @@
+
+
 $(document).ready(function() {
-  
+  $("form#questions").submit(function() {
+    event.preventDefault();
+
+    let pythonValue = 0
+    let javaValue = 0
+    let javascriptValue = 0
+  });
 });
 
 
