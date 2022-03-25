c8be59a62999efcc5c6db4ffdf168545b07d8a21
Jarret Jeter <jarretjeter@yahoo.com>
2022-03-25 14:14:09 -0700

    WIP 'scripts.js'

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,16 @@
+function resultGenerator()
+
 
 
 $(document).ready(function() {
   $("form#questions").submit(function() {
     event.preventDefault();
 
-    let pythonValue = 0
-    let javaValue = 0
-    let javascriptValue = 0
+    let pythonValue = 0; 
+    let javaValue = 0;
+    let javascriptValue = 0;
+
+    resultGenerator();
   });
 });
 
